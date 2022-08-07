79cf9f884b3b7118276c4389111b29289db1ae55
Carl Madsen <madsencarlb@gmail.com>
2022-08-07 22:14:45 +1000

    beautify the win nodes

--- tictactoe.js
+++ tictactoe.js
@@ -1,17 +1,17 @@
 const gameBoard = (() => {
     const gameSize = 3;
     const gameArray = [
-        [""],[""],[""],
-        [""],[""],[""],
-        [""],[""],[""]
+        [""], [""], [""],
+        [""], [""], [""],
+        [""], [""], [""]
     ];
+    const coordToIndex = (x, y) => x + y * 3;
     const indexToCoord = (index) => {
         return {
-            x: Number(index)%3,
-            y: Math.floor(Number(index)/3)
+            x: Number(index) % 3,
+            y: Math.floor(Number(index) / 3)
         }
     };
-    const coordToIndex = (x, y) => x + y*3;
     const getMarkerAt = (id) => gameArray[id];
     const setMarkerAt = (id, marker) => {
         gameArray[id] = marker;
@@ -21,34 +21,53 @@ const gameBoard = (() => {
         let coord = indexToCoord(cell);
         let baseX = coordToIndex(0, coord.y);
         let baseY = coordToIndex(coord.x, 0);
-        const checkHorizontal = (y) => (gameArray[y] === gameArray[y + 1] &&
-                gameArray[y + 1] === gameArray[y + 2]);
-        const checkVertical = (x) => (gameArray[x] === gameArray[x + 3] &&
-            gameArray[x + 3] === gameArray[x + 6]);
+        const checkHorizontal = (y) => {
+            if (gameArray[y] === gameArray[y + 1] && gameArray[y + 1] === gameArray[y + 2]) {
+                return [y, y + 1, y + 2];
+            };
+        }
+        const checkVertical = (x) => {
+            if (gameArray[x] === gameArray[x + 3] && gameArray[x + 3] === gameArray[x + 6]) {
+                return [x, x + 3, x + 6];
+            };
+        }
         const checkDiagonal = (mark) => {
-            return (gameArray[0] === mark && gameArray[4] === mark && gameArray[8] === mark ||
-                gameArray[2] === mark && gameArray[4] === mark && gameArray[6] === mark)
+            if(gameArray[0] === mark && gameArray[4] === mark && gameArray[8] === mark){
+                return [0, 4, 8];
+            } else if(gameArray[2] === mark && gameArray[4] === mark && gameArray[6] === mark){
+                return [2, 4, 6];
+            }
         }
-        if (checkHorizontal(baseX) || checkVertical(baseY) || checkDiagonal(marker)){
-            alert("you win");
-        }
+        return (checkHorizontal(baseX) || checkVertical(baseY) || checkDiagonal(marker));
     }
     return { gameArray, getMarkerAt, setMarkerAt, checkWin };
 })();
 
 const gameData = (() => {
     let currentPlayer;
-    const changePlayer = function(){
+    let moves = 0;
+    let winner;
+    const changePlayer = function () {
         this.currentPlayer = (this.currentPlayer === playerOne) ? playerTwo : playerOne;
         return this.currentPlayer;
     };
-    return { currentPlayer, changePlayer };
+    return { currentPlayer, changePlayer, moves };
 })();
 
 const Player = (name, marker) => {
-    const move = function(cellID, node){
+    const move = function (cellID, node) {
         node.textContent = gameBoard.setMarkerAt(cellID, this.marker);
-        gameBoard.checkWin(cellID, marker)
+        if (this.marker === "⚬"){ node.classList.add("o-sizer") };
+        node.classList.remove("grid-hover");
+        gameData.moves++;
+        let winReturn = gameBoard.checkWin(cellID, marker);
+        if (winReturn) {
+            gameData.winner = this;
+            winReturn.map(ind => document.querySelector(`#cell-${ind}`).classList.add("grid-winner"));
+            displayContainer.outputBox.textContent = `${this.name} is the winner!`;
+        } else if (gameData.moves === 9) {
+            displayContainer.outputBox.textContent = `It's a draw! Though Carl is the real winner :)`;
+        }
         gameData.changePlayer();
     }
     return { name, marker, move }
@@ -56,19 +75,23 @@ const Player = (name, marker) => {
 
 const displayContainer = (() => {
     const gridBox = document.querySelector("#grid-container");
-    for (let i = 0; i < 9; i++){
+    for (let i = 0; i < 9; i++) {
         const gridCell = document.createElement("div");
         gridCell.classList.add("grid-cell");
+        gridCell.classList.add("grid-hover");
         gridCell.id = `cell-${i}`;
         gridCell.addEventListener("mousedown", (event) => {
-            if (gameBoard.getMarkerAt(i)[0]){ return };
+            event.preventDefault();
+            if (gameData.winner) { return };
+            if (gameBoard.getMarkerAt(i)[0]) { return };
             gameData.currentPlayer.move(i, event.target);
         })
         gridBox.appendChild(gridCell);
     };
-    return { gridBox }
+    const outputBox = document.querySelector("#output-container");
+    return { gridBox, outputBox }
 })();
 
-let playerOne = Player("Carl", "x");
-let playerTwo = Player("Kindon", "o");
-gameData.changePlayer();
+let playerOne = Player("Kindon", "×");
+let playerTwo = Player("Carl", "⚬");
+gameData.currentPlayer = playerOne;
